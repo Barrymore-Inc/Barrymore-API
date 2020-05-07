975569f51fccc131f61ba46f4f5c0c95a15762b1
Artemii Bigdan <artem@bigdan.in>
2020-05-07 17:27:04 +0300

    v0.4.1
    Context for speech recognition

--- src/main/java/me/uquark/barrymore/api/BarrymoreBrain.java
+++ src/main/java/me/uquark/barrymore/api/BarrymoreBrain.java
@@ -2,7 +2,9 @@ package me.uquark.barrymore.api;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface BarrymoreBrain extends Remote {
     Order processUserMessage(Coords userCoords, String message) throws RemoteException;
+    List<String> getPhrases() throws RemoteException;
 }
